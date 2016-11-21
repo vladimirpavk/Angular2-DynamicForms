bc7bb701908f3198ee266ddac9ee359e75b6b13d
Vladimir Pavković <vladimirpavk@gmail.com>
2016-11-21 14:16:58 +0100

    Added custom validator

--- client/app/ts/app.component.ts
+++ client/app/ts/app.component.ts
@@ -10,14 +10,25 @@ export class AppComponent {
 
   public registerForm: FormGroup;
 
+  public firstnameControl: FormControl;
+  public lastnameControl: FormControl;
+  public cityControl: FormControl;
+  public usernameControl: FormControl;
+  public passwordControl: FormControl;
+  public password2Control: FormControl;
+
   constructor(private formBuilder: FormBuilder){
 
   }
 
   public ngOnInit(){
-    this.registerForm = this.formBuilder.group({
+      this._BuildForm();
+  }
+
+  _BuildForm(){
+      this.registerForm = this.formBuilder.group({
       'init': this.formBuilder.group({
-        'firstname': ['', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(15)])],
+        'firstname': ['', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(15), this.customFirstnameValidator])],
         'lastname': '',
         'city': ''
       }),
@@ -27,23 +38,42 @@ export class AppComponent {
         'password2': ['', this.customPassValidator]
       })
     });   
+
+    this.firstnameControl=<FormControl>(<FormGroup>this.registerForm.controls['init']).controls['firstname'];
+    this.lastnameControl=<FormControl>(<FormGroup>this.registerForm.controls['init']).controls['lastname'];
+    this.cityControl=<FormControl>(<FormGroup>this.registerForm.controls['init']).controls['city'];
+    this.usernameControl=<FormControl>(<FormGroup>this.registerForm.controls['cred']).controls['username'];
+    this.passwordControl=<FormControl>(<FormGroup>this.registerForm.controls['cred']).controls['password'];
+    this.password2Control=<FormControl>(<FormGroup>this.registerForm.controls['cred']).controls['password2'];
+
+
+    this.firstnameControl.statusChanges.subscribe((value)=>{
+      console.log(value);
+      //this.firstnameControl.hasError()
+    }
+    );
+   // this.firstnameControl.valueChanges.subscribe((value)=>console.log(value));
   }
 
   public clickSubmit(){
-    console.log(this.registerForm.value.cred.password2);    
+   // console.log(this.firstnameControl);   
   }
 
-  public customPassValidator(fc: FormControl){
+  public button1Click():void{
+    console.log("Promeni vrednosti");
+    this.firstnameControl.setValue("vladimir");
+  }
+
+  /*public customPassValidator(fc: FormControl): { [s: string]: boolean }{
       console.log(fc.value);
-      //console.log(this.registerForm.value.cred.password);
-      /*if(fc.value===this.registerForm.value.cred.password2){
-        //return true;
-        console.log("true");
-      }
-      else{
-        //return false;
-        console.log("false");
-      }*/
+      console.log(
+      (<FormControl>(<FormGroup>this.registerForm.controls['cred']).controls['password']).value 
+      );
+      return {"neka_greska": true};
+  }*/
+
+  public customFirstnameValidator(control: FormControl):{[s:string]: boolean}{
+    return { "nekagreska": true };
   }
 
  }
